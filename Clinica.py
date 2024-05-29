79cfd9a7df83f471b64faecd5e5535b94914cda9
Paula Castano <paula.castano1@utp.edu.co>
2024-05-28 20:20:44 -0500

    :fire: eliminar metodos comentados

--- Clinica.py
+++ Clinica.py
@@ -89,17 +89,6 @@ class Clinica:
         
         return self.planeador_de_citas.asignar_cita(fecha, medico, paciente)
 
-        
-
-    def actualizar_paciente(self, documento, nuevos_datos: dict):
-        paciente = self.obtener_paciente(documento)
-        if not paciente:
-            raise Exception('El paciente no existe')
-        
-        paciente.nombre = nuevos_datos.get('nombre', paciente.nombre)
-        paciente.apellido = nuevos_datos.get('apellido', paciente.apellido)
-        paciente.fecha_de_nacimiento = nuevos_datos.get('birth_date', paciente.fecha_de_nacimiento)
-
     def eliminar_paciente(self, documento):
         paciente = self.obtener_paciente(documento)
         if not paciente:
@@ -128,49 +117,3 @@ class Clinica:
             raise Exception("El paciente aun no tiene un historial medico")
         
         return historial_paciente.cita_actual
-
-    # def cancelar_cita(self, documento):
-    #     paciente = self.obtener_paciente(documento)
-    #     if paciente and paciente.turno_asignado:
-    #         cita = paciente.turno_asignado
-    #         cita.estado = EstadoCita.DISPONIBLE
-    #         paciente.turno_asignado = None
-    #         return True
-    #     return False
-
-    # def agregar_medico(self, nombre, horarios):
-    #     self.medicos[nombre] = horarios
-
-    # def disponibilidad_medico(self, nombre, fecha, hora):
-    #     if nombre in self.medicos:
-    #         return (fecha, hora) in self.medicos[nombre]
-    #     return False
-
-    # def ordenar_citas_por_fecha(self):
-    #     def get_key(cita):
-    #         return datetime.strptime(cita.fecha_programacion + ' ' + cita.hora_asignacion, "%Y-%m-%d %H:%M")
-        
-    #     self.citas.sort(key=get_key)
-
-    # def buscar_cita(self, fecha, hora):
-    #     def get_key(cita):
-    #         return datetime.strptime(cita.fecha_programacion + ' ' + cita.hora_asignacion, "%Y-%m-%d %H:%M")
-        
-    #     def binary_search(arr, x):
-    #         l = 0
-    #         r = len(arr) - 1
-    #         while l <= r:
-    #             mid = l + (r - l) // 2
-    #             mid_date = get_key(arr[mid])
-    #             x_date = datetime.strptime(x, "%Y-%m-%d %H:%M")
-    #             if mid_date == x_date:
-    #                 return arr[mid]
-    #             elif mid_date < x_date:
-    #                 l = mid + 1
-    #             else:
-    #                 r = mid - 1
-    #         return None
-        
-    #     self.ordenar_citas_por_fecha()
-    #     cita_buscada = binary_search(self.citas, f"{fecha} {hora}")
-    #     return cita_buscada
